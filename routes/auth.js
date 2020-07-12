ca6e4b1affd1b5b5c65aa0571f4111c703295620
You Song Hou <yousonghou@yahoo.com>
2020-07-11 22:19:49 -0400

    add a route to login the user

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,32 @@ router.post('/register', async (req, res) => {
 
         const dataUser = await User.create(newUser);
 
-        return res.status(200).json({ data: dataUser });
+        return res.status(201).json({ data: dataUser });
+    } catch(err){
+        console.error(err);
+    }
+});
+
+// PUT /api/auth/login
+// login user
+router.put('/login', async (req, res) => {
+    try{
+        const user = await User.findOne({ email: req.body.email });
+
+        if(!user){
+            return res.status(400).json({ errors: 'Email not found' });
+        }
+
+        if(req.body.password != user.password){
+            return res.status(400).json({ errors: 'Invalid email or password' });
+        }
+
+        const loginData = {
+            name: user.name,
+            email: user.email,
+        };
+
+        return res.status(200).json({ data: loginData });
     } catch(err){
         console.error(err);
     }
