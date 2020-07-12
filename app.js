bdeaeffbcc68b005507782eca4773cdbdf2cf272
You Song Hou <yousonghou@yahoo.com>
2020-07-12 00:29:21 -0400

    config passport

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const db_key = require('./config/keys').mongoURI;
 
@@ -8,6 +9,9 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.get('/', (req, res) => res.send('Server Work'));
 
