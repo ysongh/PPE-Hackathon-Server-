917201b22ef3e73510c7b0235a81ed2a46736f5c
You Song Hou <yousonghou@yahoo.com>
2020-07-12 01:41:32 -0400

    set it up for deployment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const cors = require('cors');
 
 const db_key = require('./config/keys').mongoURI;
 
@@ -9,6 +10,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use(cors())
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
